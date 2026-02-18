66b10d6adb229822cd01d06a574c037850b27d91
Josh Tacker <joshtacker@Joshs-MacBook-Pro.local>
2026-02-17 21:39:46 -0500

    final product

--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@
 #
 library(shiny)
 library(shinydashboard)
-library(tidyverse)
+library(shinyWidgets)
 
 ui <- dashboardPage(
   dashboardHeader(title = "Credit Risk Assessment"),
@@ -21,15 +21,29 @@ ui <- dashboardPage(
     fluidRow(
       column(
         width = 6,
-        numericInput("total_income", "Total Income", 60000),
-        numericInput("total_dti", "Debt-to-Income (%)", 20),
-        numericInput("loan_amount", "Loan Amount", 15000)
+        numericInputIcon(
+          inputId = "total_income",
+          label = "Total Income",
+          value = 60000,
+          min = 0,
+          step = 1000,
+          icon = list(NULL, icon("dollar-sign"))
+        ),
+        numericInput("total_dti", "Debt-to-Income Ratio (%)", 20),
+        numericInputIcon(
+          inputId = "loan_amount",
+          label = "Loan Amount",
+          value = 15000,
+          min = 0,
+          step = 500,
+          icon = list(NULL, icon("dollar-sign"))
+        )
       ),
       column(
         width = 6,
         selectInput("term", "Loan Term", choices = levels(credit_model$term)),
         numericInput("credit_history_years", "Credit History (Years)", 10),
-        numericInput("delinq_2y", "Delinquencies (2Y)", 0)
+        numericInput("delinq_2y", "Delinquencies (2 Years)", 0)
       )
     ),
     
@@ -37,14 +51,14 @@ ui <- dashboardPage(
     fluidRow(
       column(
         width = 6,
-        numericInput("months_since_last_delinq", "Months Since Last Delinq", 999),
-        numericInput("months_since_90d_late", "Months Since 90D Late", 999),
+        numericInput("months_since_last_delinq", "Months Since Last Delinquency", 999),
+        numericInput("months_since_90d_late", "Months Since 90+ Day Late", 999),
         numericInput("num_historical_failed_to_pay", "Historical Failures", 0)
       ),
       column(
         width = 6,
-        numericInput("inquiries_last_12m", "Inquiries (12M)", 2),
-        numericInput("credit_utilization", "Credit Utilization", 0.3),
+        numericInput("inquiries_last_12m", "Credit Inquiries (12M)", 2),
+        numericInput("credit_utilization", "Credit Utilization (%)", 30),
         numericInput("total_credit_lines", "Total Credit Lines", 10)
       )
     ),
@@ -77,7 +91,7 @@ ui <- dashboardPage(
         title = "Top Risk Drivers",
         width = 12,
         tableOutput("explain_table"),
-        plotOutput("waterfall_plot", height = "400px")  # <-- Waterfall chart
+        plotOutput("waterfall_plot", height = "400px")  # Waterfall chart
       )
     )
   )
